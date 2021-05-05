bc07ec36a2def42f915e442a52fc8f53f95ce305
v3vgroups <83592713+v3vgroups@users.noreply.github.com>
2021-05-05 21:40:55 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$gender= $_POST['gender'];
+$age_group= $_POST['Age_group'];
+$Feedback=$_POST['Feedback'];
+$to = "muhdadnankhan786@gmail.com";
+$subject = "Mail From youtube website";
+$txt ="Name = ". $name . "\r\n  Gender = " . $gender . "\r\n AGE_GROUP =" . $age_group "\r\n Feedback =" . $Feedback;
+$headers = "From: youtube@gmail.com.com" . "\r\n" .
+"CC: muhdadnankhan786123786@gmail.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("indexmail.html");
+?>
